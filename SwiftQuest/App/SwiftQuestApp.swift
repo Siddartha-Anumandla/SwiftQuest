90d0891e9b0dd1083bb3ef154149c496b556afd7
siddartha anumandla <siddarthaanumandla@Siddarthas-MacBook-Air.local>
2026-07-09 22:20:52 +0530

    chore: setup initial project architecture

---
+++ SwiftQuest/App/SwiftQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftQuestApp.swift
+//  SwiftQuest
+//
+//  Created by Siddartha Anumandla on 09/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftQuestTests/SwiftQuestTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftQuestTests.swift
+//  SwiftQuestTests
+//
+//  Created by Siddartha Anumandla on 09/07/26.
+//
+
+import Testing
+@testable import SwiftQuest
+
+struct SwiftQuestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
